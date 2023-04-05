a88fcaec8be2a71f8a248602fd9fa1cb3c8cdf8f
devonsoto77 <online.devon@gmail.com>
2023-04-05 00:13:22 -0600

    move mockdate to inline

--- components/timeline/timeline.tsx
+++ components/timeline/timeline.tsx
@@ -21,20 +21,54 @@ export const Timeline = () => {
   return (
     <div className={styles.timeline}>
       <h1 className={styles.experiences}>Experiences</h1>
+      <p>
+        download my resume{" "}
+        <a href="your_resume.pdf" download>
+          <button>Download Resume</button>
+        </a>
+      </p>
       <div className={styles.container}>
-        {/* <div className={styles.verticalbar} /> */}
-        {contentList.map((info, index) => (
-          <>
-            {/* <div className={styles.leftsideContainer}></div> */}
-            <div className={styles.rightContainer} key={index}>
-              {/* <div className={styles.circle} /> */}
-              <p className={styles.title}>{contentList[index].title}</p>
-              <p className={styles.description}>
-                {contentList[index].description}
-              </p>
-            </div>
-          </>
-        ))}
+        <div className={styles.rightContainer}>
+          <p className={styles.title}>Software Engineer at Workday</p>
+          <p className={styles.date}>2020 - Current</p>
+          <p className={styles.description}>
+            I am incredibly grateful for the opportunity to work at Workday,
+            where I had the privilege of contributing to the development of
+            multiple products that were used by a wide range of clients.
+          </p>
+        </div>
+        <div className={styles.rightContainer}>
+          {/* <div className={styles.circle} /> */}
+          <p className={styles.title}>Software Engineer Intern at Workday</p>
+          <p className={styles.date}>2017 - 2020</p>
+          <p className={styles.description}>
+            I am incredibly grateful for the opportunity to work at Workday,
+            where I had the privilege of contributing to the development of
+            multiple products that were used by a wide range of clients.
+          </p>
+        </div>
+        <div className={styles.rightContainer}>
+          {/* <div className={styles.circle} /> */}
+          <p className={styles.title}>
+            Sr Associate Software Development Engineer
+          </p>
+          <p className={styles.description}>
+            I am incredibly grateful for the opportunity to work at Workday,
+            where I had the privilege of contributing to the development of
+            multiple products that were used by a wide range of clients.
+          </p>
+        </div>
+        <div className={styles.rightContainer}>
+          {/* <div className={styles.circle} /> */}
+          <p className={styles.title}>
+            Sr Associate Software Development Engineer
+          </p>
+          <p className={styles.description}>
+            I am incredibly grateful for the opportunity to work at Workday,
+            where I had the privilege of contributing to the development of
+            multiple products that were used by a wide range of clients.
+          </p>
+        </div>
       </div>
     </div>
   );
@@ -48,3 +82,15 @@ export default Timeline;
 // description
 // time
 // bullet points
+// {contentList.map((info, index) => (
+//   <>
+//     {/* <div className={styles.leftsideContainer}></div> */}
+// <div className={styles.rightContainer} key={index}>
+//   {/* <div className={styles.circle} /> */}
+//   <p className={styles.title}>{contentList[index].title}</p>
+//   <p className={styles.description}>
+//     {contentList[index].description}
+//   </p>
+// </div>
+//   </>
+// ))}
