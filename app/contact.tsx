566d25a8e61ebb816b8326f77ccbe9c7e07ffd63
devonsoto77 <online.devon@gmail.com>
2023-07-12 18:43:30 -0600

    stuff

--- app/contact.tsx
+++ app/contact.tsx
@@ -1,69 +1,99 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Contact() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [message, setMessage] = useState("");
+
+  const postUsers = async () => {
+    fetch("https://api.postmarkapp.com/email", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Postmark-Server-Token": "f96c53cf-e0eb-43f4-872d-09f6c4fbe9a1",
+      },
+      body: JSON.stringify({ firstName, lastName, message }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+
   return (
-    <div id='contact' className='relative isolate pb-24 sm:py-32 text-sec'>
-      <div className='mx-auto max-w-xl lg:max-w-4xl'>
-        <h2 className='text-4xl font-bold tracking-tight'>
+    <div id="contact" className="relative isolate pb-24 sm:py-32 text-sec">
+      <div className="mx-auto max-w-xl lg:max-w-4xl">
+        <h2 className="text-4xl font-bold tracking-tight">
           Let’s talk about your project
         </h2>
-        <div className='mt-16 flex flex-col gap-16 sm:gap-y-20 lg:flex-row'>
-          <form action='#' method='POST' className='lg:flex-auto'>
-            <div className='grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2'>
+        <div className="mt-16 flex flex-col gap-16 sm:gap-y-20 lg:flex-row">
+          <form action="#" method="POST" className="lg:flex-auto">
+            <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
               <div>
                 <label
-                  htmlFor='first-name'
-                  className='block text-sm font-semibold leading-6 '
+                  htmlFor="first-name"
+                  className="block text-sm font-semibold leading-6 "
                 >
                   First name
                 </label>
-                <div className='mt-2.5'>
+                <div className="mt-2.5">
                   <input
-                    type='text'
-                    name='first-name'
-                    id='first-name'
-                    autoComplete='given-name'
-                    className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                    type="text"
+                    name="first-name"
+                    value={firstName}
+                    onChange={(event) => setFirstName(event?.target.value)}
+                    id="first-name"
+                    autoComplete="given-name"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
               <div>
                 <label
-                  htmlFor='last-name'
-                  className='block text-sm font-semibold leading-6 '
+                  htmlFor="last-name"
+                  className="block text-sm font-semibold leading-6 "
                 >
                   Last name
                 </label>
-                <div className='mt-2.5'>
+                <div className="mt-2.5">
                   <input
-                    type='text'
-                    name='last-name'
-                    id='last-name'
-                    autoComplete='family-name'
-                    className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                    type="text"
+                    name="last-name"
+                    value={lastName}
+                    onChange={(event) => setLastName(event?.target.value)}
+                    id="last-name"
+                    autoComplete="family-name"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
-              <div className='sm:col-span-2'>
+              <div className="sm:col-span-2">
                 <label
-                  htmlFor='message'
-                  className='block text-sm font-semibold leading-6 '
+                  htmlFor="message"
+                  className="block text-sm font-semibold leading-6 "
                 >
                   Message
                 </label>
-                <div className='mt-2.5'>
+                <div className="mt-2.5">
                   <textarea
-                    id='message'
-                    name='message'
+                    id="message"
+                    name="message"
                     rows={4}
-                    className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-                    defaultValue={''}
+                    value={message}
+                    onChange={(event) => setMessage(event?.target.value)}
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    defaultValue={""}
                   />
                 </div>
               </div>
             </div>
-            <div className='mt-10'>
+            <div className="mt-10">
               <button
-                type='submit'
-                className='block w-full rounded-md bg-sec px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-prim focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+                onClick={(event) => {
+                  event.preventDefault();
+                  postUsers();
+                }}
+                className="block w-full rounded-md bg-sec px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-prim focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Let’s talk
               </button>
@@ -72,5 +102,5 @@ export default function Contact() {
         </div>
       </div>
     </div>
-  )
+  );
 }
